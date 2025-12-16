931b067f272f492b3e4608f1e868b35fbc81f4f0
Đỗ Gia <giado280904@gmail.com>
2025-12-17 02:36:40 +0700

    sửa giao diện đăng nhập, xóa bỏ đăng ký

--- routes/web.php
+++ routes/web.php
@@ -130,8 +130,6 @@ Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 
 Route::post('login', [LoginController::class, 'login']);
 
-Route::post('/register-nhanvien', [LoginController::class, 'storeNhanVien'])->name('register.store');
-
 Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 
 
