5dbb453e0e45bfdf3897d1f813a8b109156bde0e
Tên của bạn <trungpldph51651@pldph51651gmail.51651gmailcom>
2025-12-22 22:07:59 +0700

    code

--- resources/views/admins/hoa-don/index.blade.php
+++ resources/views/admins/hoa-don/index.blade.php
@@ -6,9 +6,7 @@
 <main class="app-content">
     <div class="app-title d-flex justify-content-between align-items-center mb-4">
         <h1><i class="fa fa-file-invoice"></i> Danh sách hóa đơn</h1>
-        <a href="{{ route('admin.hoa-don.create') }}" class="btn btn-primary">
-            <i class="fa fa-plus"></i> Tạo hóa đơn mới
-        </a>
+
     </div>
 
     @if(session('success'))
